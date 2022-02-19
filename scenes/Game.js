ee34b447540bd191c2fbffe8f81d705d035a51b3
Cordell <cordellbonnieux@gmail.com>
2022-02-18 22:59:36 -0800

    added controls.

--- scenes/Game.js
+++ scenes/Game.js
@@ -17,5 +17,26 @@ export default class Game extends Phaser.Scene {
         const paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff, 1)
         this.physics.add.existing(paddleLeft, true)
         this.physics.add.collider(paddleLeft, ball)
+
+
+        const paddleRight = this.add.rectangle(750, 250, 20, 100, 0xffffff, 1)
+        this.physics.add.existing(paddleRight, true)
+        this.physics.add.collider(paddleRight, ball)
+
+        this.input.keyboard.on('keydown-W', (e) => {
+            if (paddleLeft.y > 50) {
+                paddleLeft.setY(paddleLeft.y - 10)
+            }
+        })
+
+        this.input.keyboard.on('keydown-S', (e) => {
+            if (paddleLeft.y < 450) {
+                paddleLeft.setY(paddleLeft.y + 10)
+            }
+        })
+    }
+
+    update() {
+
     }
 }
