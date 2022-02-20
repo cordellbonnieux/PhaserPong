2ccd98b7046d26240c3a7ea24e7ebcaa404e20b1
Cordell <cordellbonnieux@gmail.com>
2022-02-19 17:00:24 -0800

    Replaced lambda expressions in create() used for controlling the paddle, with data member variables which are modified in update()  allowing for smoother movement.

--- scenes/Game.js
+++ scenes/Game.js
@@ -1,42 +1,87 @@
 import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
+    constructor() {
+        super()
+        this.ball
+        this.paddleLeft
+        this.randX
+        this.randY
+        this.moveUp = false
+        this.moveDown = false
+    }
     preload() {
-
+        this.paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff, 1)
+        this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+        this.randX = -200 //this.getRand()
+        this.randY = 0 //this.getRand()
     }
 
     create() {
-        const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-        this.physics.add.existing(ball)
-        ball.body.setBounce(1, 1)
+        // ball
+        this.physics.add.existing(this.ball)
+        this.ball.body.setBounce(1, 1)
+        this.ball.body.setCollideWorldBounds(true, 1, 1)
+        this.ball.body.setVelocity(this.randX, this.randY)
 
-        ball.body.setCollideWorldBounds(true, 1, 1)
+        // paddle left
+        this.physics.add.existing(this.paddleLeft, true)
 
-        ball.body.setVelocity(-200, 0)
+        // colliders
+        this.physics.add.collider(this.paddleLeft, this.ball)
 
-        const paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff, 1)
-        this.physics.add.existing(paddleLeft, true)
-        this.physics.add.collider(paddleLeft, ball)
-
+        /*
+        this.physics.add.collider(paddleLeft, ball, (p, b) => {
+            console.log('ball collided with left paddle')
+        })
 
         const paddleRight = this.add.rectangle(750, 250, 20, 100, 0xffffff, 1)
         this.physics.add.existing(paddleRight, true)
-        this.physics.add.collider(paddleRight, ball)
+        this.physics.add.collider(paddleRight, ball, (p, b) => {
+            console.log('ball collided with right paddle')
+        })
+        */
 
+        // add smooth movement
+        this.input.keyboard.on('keydown-W', () => this.moveUp = true)
+        this.input.keyboard.on('keyup-W', () => this.moveUp = false)
+        this.input.keyboard.on('keydown-S', () => this.moveDown = true)
+        this.input.keyboard.on('keyup-S', () => this.moveDown = false)
+    }
+
+    update() {
+        // add controls
+        if (this.moveUp && this.paddleLeft.y > 50) {
+            this.paddleLeft.y -= 10
+        }
+        if (this.moveDown && this.paddleLeft.y < 450) {
+            this.paddleLeft.y += 10
+        }
+        this.paddleLeft.body.updateFromGameObject() 
+        /*
         this.input.keyboard.on('keydown-W', (e) => {
-            if (paddleLeft.y > 50) {
-                paddleLeft.setY(paddleLeft.y - 10)
+            if (this.paddleLeft.y > 50) {
+                //this.paddleLeft.setY(this.paddleLeft.y - 10)
+                this.paddleLeft.y -= 1
+                this.paddleLeft.body.updateFromGameObject()
             }
         })
 
         this.input.keyboard.on('keydown-S', (e) => {
-            if (paddleLeft.y < 450) {
-                paddleLeft.setY(paddleLeft.y + 10)
+            if (this.paddleLeft.y < 450) {
+                //this.paddleLeft.setY(this.paddleLeft.y + 10)
+                this.paddleLeft.y += 1
+                this.paddleLeft.body.updateFromGameObject()
             }
         })
+        */
     }
 
-    update() {
+    render() {
 
+    }
+
+    getRand() {
+        return Phaser.Math.Between(-200, 200)
     }
 }
