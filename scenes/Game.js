f4cced2d56405cb133f80c03cbd607a21f889f16
Cordell <cordellbonnieux@gmail.com>
2022-02-20 21:49:10 -0800

    Added a pause menu

--- scenes/Game.js
+++ scenes/Game.js
@@ -3,10 +3,11 @@ import Phaser from 'phaser'
 export default class Game extends Phaser.Scene {
     constructor() {
         super()
-        this.playing
         this.ball
         this.paddleLeft
         this.paddleRight
+        this.pauseMenu
+        this.playing = false
         this.moveUp = false
         this.moveDown = false
         this.score = {
@@ -16,27 +17,27 @@ export default class Game extends Phaser.Scene {
         }
     }
     preload() {
+        // create game objects
         this.paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff, 1)
         this.paddleRight = this.add.rectangle(750, 250, 20, 100, 0xffffff, 1)
         this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+
+        // create ui elements
         this.score.text = this.add.text(290, 10, `Player: ${this.score.player} Computer: ${this.score.ai}`)
-
-        // maybe start with the screen?
-        this.playing = true
+        this.pauseMenu = this.add.text(250, 350, 'click anywhere to start the round')
     }
-
     create() {
-        // ball
+        // ball physics
         this.physics.add.existing(this.ball)
         this.ball.body.setBounce(1, 1)
         this.ball.body.setCollideWorldBounds(true, 1, 1)
         this.ball.body.setVelocity(-(this.getRand()), this.getRand())
 
-        // add paddles
+        // paddle physics
         this.physics.add.existing(this.paddleLeft, true)
         this.physics.add.existing(this.paddleRight, true)
 
-        // colliders
+        // paddle and ball colliders
         this.physics.add.collider(this.paddleLeft, this.ball)
         this.physics.add.collider(this.paddleRight, this.ball)
 
@@ -46,11 +47,9 @@ export default class Game extends Phaser.Scene {
         this.input.keyboard.on('keydown-S', () => this.moveDown = true)
         this.input.keyboard.on('keyup-S', () => this.moveDown = false)
     }
-
     update() {
         // while the game is in playing mode
         if (this.playing) {
-
             // handle player input
             if (this.moveUp && this.paddleLeft.y > 50) {
                 this.paddleLeft.y -= 10
@@ -83,13 +82,19 @@ export default class Game extends Phaser.Scene {
                 this.score.text.setText(`Player: ${this.score.player} Computer: ${this.score.ai}`)
                 this.playing = false
             }
+        // while the game is paused 
         } else {
             // freeze and center the ball
             this.ball.body.setVelocity(0, 0)
             this.ball.body.x = 400
             this.ball.body.y = 250
-                // spawn 'click anywhere for next round' box
-                // add event listener, then hide it
+
+            // recenter the paddles
+            this.paddleLeft.body.y = 250
+            this.paddleRight.body.y = 250
+
+            // spawn 'click anywhere for next round' box
+            // add event listener, then hide it
         }
 
     }
