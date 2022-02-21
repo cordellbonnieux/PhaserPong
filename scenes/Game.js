c99544de475ab8ea9bf3f6dff5384049f281f6fb
Cordell <cordellbonnieux@gmail.com>
2022-02-20 21:40:59 -0800

    Game now stops when player or ai scores a point.

--- scenes/Game.js
+++ scenes/Game.js
@@ -3,16 +3,26 @@ import Phaser from 'phaser'
 export default class Game extends Phaser.Scene {
     constructor() {
         super()
+        this.playing
         this.ball
         this.paddleLeft
         this.paddleRight
         this.moveUp = false
         this.moveDown = false
+        this.score = {
+            player: 0,
+            ai: 0,
+            text: ''
+        }
     }
     preload() {
         this.paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff, 1)
         this.paddleRight = this.add.rectangle(750, 250, 20, 100, 0xffffff, 1)
         this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+        this.score.text = this.add.text(290, 10, `Player: ${this.score.player} Computer: ${this.score.ai}`)
+
+        // maybe start with the screen?
+        this.playing = true
     }
 
     create() {
@@ -38,26 +48,50 @@ export default class Game extends Phaser.Scene {
     }
 
     update() {
-        // handle player input
-        if (this.moveUp && this.paddleLeft.y > 50) {
-            this.paddleLeft.y -= 10
-        }
-        if (this.moveDown && this.paddleLeft.y < 450) {
-            this.paddleLeft.y += 10
-        }
-        this.paddleLeft.body.updateFromGameObject()
+        // while the game is in playing mode
+        if (this.playing) {
 
-        // AI paddle to follow ball
-        const num = Phaser.Math.Between(1,100)
-        const move = num > 70 ? true : false
-        if (move && this.ball.x > 400 && this.ball.body.velocity.x > 0) {
-            if (this.paddleRight.y > this.ball.y) {
-                this.paddleRight.y -= 5
-            } else if (this.paddleRight.y < this.ball.y) {
-                this.paddleRight.y += 5
+            // handle player input
+            if (this.moveUp && this.paddleLeft.y > 50) {
+                this.paddleLeft.y -= 10
             }
-            this.paddleRight.body.updateFromGameObject()
+            if (this.moveDown && this.paddleLeft.y < 450) {
+                this.paddleLeft.y += 10
+            }
+            this.paddleLeft.body.updateFromGameObject()
+
+            // AI paddle to follow ball
+            const num = Phaser.Math.Between(1,100)
+            const move = num > 50 ? true : false
+            if (move && this.ball.x > 400 && this.ball.body.velocity.x > 0) {
+                if (this.paddleRight.y > this.ball.y) {
+                    this.paddleRight.y -= 5
+                } else if (this.paddleRight.y < this.ball.y) {
+                    this.paddleRight.y += 5
+                }
+                this.paddleRight.body.updateFromGameObject()
+            }
+
+            // adjust score based on ball pos
+            if (this.ball.body.x == 0 || this.ball.body.x == 800) {
+                if (this.ball.body.x == 0) {
+                    this.score.ai += 1
+                } else if (this.ball.body.x == 800) {
+                    this.score.player += 1
+                }
+                // update score
+                this.score.text.setText(`Player: ${this.score.player} Computer: ${this.score.ai}`)
+                this.playing = false
+            }
+        } else {
+            // freeze and center the ball
+            this.ball.body.setVelocity(0, 0)
+            this.ball.body.x = 400
+            this.ball.body.y = 250
+                // spawn 'click anywhere for next round' box
+                // add event listener, then hide it
         }
+
     }
 
     getRand() {
